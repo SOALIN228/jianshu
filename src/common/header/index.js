13aa037ce493d9cc5cf0d86ea980a22877f7c030
lml <soalin228@163.com>
2019-08-04 19:41:46 +0800

    代码优化，解构赋值，使用switch代替if

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -21,7 +21,8 @@ import {
 class Header extends Component {
 
   getListArea () {
-    if (this.props.focused) {
+    const { focused, list } = this.props
+    if (focused) {
       return (
         <SearchInfo>
           <SearchInfoTitle>
@@ -30,7 +31,7 @@ class Header extends Component {
           </SearchInfoTitle>
           <SearchInfoList>
             {
-              this.props.list.map((item) => {
+              list.map((item) => {
                 return <SearchInfoItem key={item}>{item}</SearchInfoItem>
               })
             }
@@ -43,6 +44,7 @@ class Header extends Component {
   }
 
   render () {
+    const { focused, handleInputFocus, handleInputBlur } = this.props
     return (
       <HeaderWrapper>
         <Login/>
@@ -54,16 +56,16 @@ class Header extends Component {
             <i className={'iconfont'}>&#xe666;</i>
           </NavItem>
           <SearchWrapper>
-            <CSSTransition in={this.props.focused}
+            <CSSTransition in={focused}
                            timeout={200}
                            classNames="slide"
             >
-              <NavSearch className={this.props.focused ? 'focused' : ''}
-                         onFocus={this.props.handleInputFocus}
-                         onBlur={this.props.handleInputBlur}
+              <NavSearch className={focused ? 'focused' : ''}
+                         onFocus={handleInputFocus}
+                         onBlur={handleInputBlur}
               />
             </CSSTransition>
-            <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>&#xe602;</i>
+            <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe602;</i>
             {this.getListArea()}
           </SearchWrapper>
         </Nav>
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -7,15 +7,14 @@ const defaultStatus = fromJS({
 })
 
 export default (state = defaultStatus, action) => {
-  if (action.type === constants.SEARCH_FOCUS) {
-    // immutable对象的set方法，会结果之前immutable对象的值，返回新的对象
-    return state.set('focused', true)
+  switch (action.type) {
+    case constants.SEARCH_FOCUS:
+      return state.set('focused', true) // immutable对象的set方法，会结果之前immutable对象的值，返回新的对象
+    case constants.SEARCH_BLUR:
+      return state.set('focused', false)
+    case constants.CHANGE_LIST:
+      return state.set('list', action.data)
+    default:
+      return state
   }
-  if (action.type === constants.SEARCH_BLUR) {
-    return state.set('focused', false)
-  }
-  if (action.type === constants.CHANGE_LIST) {
-    return state.set('list', action.data)
-  }
-  return state
 }
