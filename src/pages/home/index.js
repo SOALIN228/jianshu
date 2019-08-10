749bb4ffda5a7a5c76109211b4b872922cad87c5
lml <soalin228@163.com>
2019-08-10 23:01:21 +0800

    回到顶部完成

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,10 +8,16 @@ import Writer from './components/Writer'
 import {
   HomeWrapper,
   HomeLeft,
-  HomeRight
+  HomeRight,
+  BackTop
 } from './style'
 
 class Home extends Component {
+
+  static handleScrollTop () {
+    window.scrollTo(0, 0)
+  }
+
   render () {
     return (
       <HomeWrapper>
@@ -26,20 +32,40 @@ class Home extends Component {
           <Recommend/>
           <Writer/>
         </HomeRight>
+        {this.props.showScroll ? <BackTop onClick={Home.handleScrollTop}>顶部</BackTop> : null}
       </HomeWrapper>
     )
   }
 
+  bindEvents () {
+    window.addEventListener('scroll', this.props.changeScrollTopShow)
+  }
+
   componentDidMount () {
     this.props.changeHomeData()
+    this.bindEvents()
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.props.changeScrollTopShow)
   }
 }
 
+const mapStateToProps = (state) => ({
+  showScroll: state.getIn(['home', 'showScroll'])
+})
+
 const mapDispatchToProps = (dispatch) => ({
   changeHomeData () {
-    const action = actionCreators.getHomeInfo()
-    dispatch(action)
+    dispatch(actionCreators.getHomeInfo())
+  },
+  changeScrollTopShow () {
+    if (document.documentElement.scrollTop > 100) {
+      dispatch(actionCreators.toggleTopShow(true))
+    } else {
+      dispatch(actionCreators.toggleTopShow(false))
+    }
   }
 })
 
-export default connect(null, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
