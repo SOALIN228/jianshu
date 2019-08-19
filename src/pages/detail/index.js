f560a107e1a255cd4eadad4b8528696b9bff992b
lml <soalin228@163.com>
2019-08-19 15:11:04 +0800

    详情页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { DetailWrapper, Header, Content } from './style'
 
 class Detail extends Component {
   render () {
     return (
-      <div>detail</div>
+      <DetailWrapper>
+        <Header>Python制作快递查询系统，再一次感受到了Python的强大!</Header>
+        <Content>
+          <img src="//upload-images.jianshu.io/upload_images/14581851-4472c1ad81e915f3?imageMogr2/auto-orient/strip%7CimageView2/2/w/640/format/webp" alt=""/>
+          <p><b>今天想发个简(qi)单(shi)的(jiu)练(shi)手(xiang)小(tou)项(ge)目(lan)</b>~利用Python制作一个简单的快递查询系统。</p>
+          <p>今天想发个简(qi)单(shi)的(jiu)练(shi)手(xiang)小(tou)项(ge)目(lan)~利用Python制作一个简单的快递查询系统。</p>
+          <p>今天想发个简(qi)单(shi)的(jiu)练(shi)手(xiang)小(tou)项(ge)目(lan)~利用Python制作一个简单的快递查询系统。</p>
+          <p>今天想发个简(qi)单(shi)的(jiu)练(shi)手(xiang)小(tou)项(ge)目(lan)~利用Python制作一个简单的快递查询系统。</p>
+          <p>今天想发个简(qi)单(shi)的(jiu)练(shi)手(xiang)小(tou)项(ge)目(lan)~利用Python制作一个简单的快递查询系统。</p>
+        </Content>
+      </DetailWrapper>
     )
   }
 }
